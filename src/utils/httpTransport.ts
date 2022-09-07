de815d82dbb9b5db908d9015a384435af2be0e1c
Alex <atrepyev27@yandex.ru>
2022-09-07 09:35:46 +0300

    feat: implement XMLHttpRequest

---
+++ src/utils/httpTransport.ts
@@ -0,0 +1,64 @@
+enum METHOD {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE'
+};
+
+type Options = {
+  method: METHOD;
+  data?: any;
+};
+
+type OptionsWithoutMethod = Omit<Options, 'method'>;
+
+function queryStringify(data: object) {
+  const keys = Object.keys(data);
+  
+  return keys.reduce((result, key, index) => {
+    return `${result}${key}=${data[key]}${index < keys.length - 1 ? '&' : ''}`;
+  }, '?');
+}
+
+export default class HTTPTransport {
+  get(url: string, options: OptionsWithoutMethod = {}): Promise<XMLHttpRequest> {
+    return this.request(url, { ...options, method: METHOD.GET });
+  }
+  
+  post = (url: string, options: OptionsWithoutMethod = {}): Promise<XMLHttpRequest> => {
+    return this.request(url, { ...options, method: METHOD.POST });
+  };
+  
+  put = (url: string, options: OptionsWithoutMethod = {}): Promise<XMLHttpRequest> => {
+    return this.request(url, { ...options, method: METHOD.PUT });
+  };
+  
+  delete = (url: string, options: OptionsWithoutMethod = {}): Promise<XMLHttpRequest> => {
+    return this.request(url, { ...options, method: METHOD.DELETE });
+  };
+  
+  request = (url: string, options: Options = { method: METHOD.GET }): Promise<XMLHttpRequest> => {
+    const { method, data } = options;
+    
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      const isGet = method === METHOD.GET;
+      
+      xhr.open(method, isGet && !!data ? `${url}${queryStringify(data)}` : url);
+      
+      xhr.onload = function() {
+        resolve(xhr);
+      };
+      
+      xhr.onabort = reject;
+      xhr.onerror = reject;
+      xhr.ontimeout = reject;
+      
+      if (isGet || !data) {
+        xhr.send();
+      } else {
+        xhr.send(data);
+      }
+    });
+  }
+};
